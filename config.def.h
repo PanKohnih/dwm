233032faf84f67f781c7b3f1203accb55d8316cd
PanKohnih <flotocki002@gmail.com>
2020-08-20 23:19:14 +0200

    change browser and terminal
    changed browser from brave-beta to firefox-developer-edition
    
    removed incognito (browser change)
    
    changed terminal from st to alacritty

--- config.def.h
+++ config.def.h
@@ -73,9 +73,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_darkred, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *bravebeta[] = { "brave-beta", NULL };
-static const char *bravebetaincognito[] = { "brave-beta --incognito", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browser[] = { "firefox-developer-edition", NULL };
 static const char *emojis[] = { "dmenuunicode", NULL };
 
 static Key keys[] = {
@@ -84,8 +83,7 @@ static Key keys[] = {
 	{ MODKEY,	             	XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_e,	   spawn,	   {.v = emojis } },
 	{ MODKEY,                       XK_n,      togglebar,      {0} },
-	{ MODKEY,			XK_b,	   spawn,	   {.v = bravebeta } },
-	{ MODKEY|ShiftMask,		XK_b,	   spawn,	   {.v = bravebetaincognito } },
+	{ MODKEY,			XK_b,	   spawn,	   {.v = browser } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
